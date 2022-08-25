2efbdf4e09b079037b2cb5ff10f4cbfafbc81ef1
axelio <cakirburak43@hotmail.com>
2022-08-25 17:45:45 -0500

    bug fix on text-to speech + css adjustment on homo

--- results/results.js
+++ results/results.js
@@ -1,5 +1,10 @@
 window.onload = (e) => {
     const synth = window.speechSynthesis;
+    // cancel the speech when page closed
+    window.addEventListener("beforeunload", function() {
+        synth.cancel(); 
+    })
+    
     loadRecipe(synth);
 }
 
